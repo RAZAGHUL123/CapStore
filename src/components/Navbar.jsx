621d833edc2c11c64623c2bf71cc03db9cd1c601
RAZAGHUL123 <92047284+RAZAGHUL123@users.noreply.github.com>
2023-10-02 20:19:41 -0700

    read

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from './CartContext';
+import { Navbar, Nav } from 'react-bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Navbar({ token, setToken }) {
+function CustomNavbar({ token, setToken }) {
   const { cart } = useCart();
   const cartCount = cart.reduce((total, item) => total + item.count, 0);
 
@@ -12,56 +14,31 @@ function Navbar({ token, setToken }) {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Home</Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/cart">Cart ({cartCount})</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/store">Store</Link> {/* Store link appears for everyone */}
-          </li>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
+          <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          <Nav.Link as={Link} to="/cart">Cart ({cartCount})</Nav.Link>
+          <Nav.Link as={Link} to="/store">Store</Nav.Link>
+
           {token ? (
             <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/dashboard">Dashboard</Link> {/* Dashboard link */}
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/logout" onClick={handleLogout}>Logout</Link>
-              </li>
+              <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+              <Nav.Link as={Link} to="/logout" onClick={handleLogout}>Logout</Nav.Link>
             </>
           ) : (
             <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/signup">Signup</Link>
-              </li>
+              <Nav.Link as={Link} to="/login">Login</Nav.Link>
+              <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
             </>
           )}
-        </ul>
-      </div>
-    </nav>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
