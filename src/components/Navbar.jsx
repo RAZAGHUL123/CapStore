09d70d698d1a8abe8348dd38943b6b3f5b00aa49
RAZAGHUL123 <92047284+RAZAGHUL123@users.noreply.github.com>
2023-10-02 21:39:58 -0700

    read

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from './CartContext';
 import { Navbar, Nav } from 'react-bootstrap'; 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function CustomNavbar({ token, setToken }) {
